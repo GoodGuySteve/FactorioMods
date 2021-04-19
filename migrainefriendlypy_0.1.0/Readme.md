9fb438b7845b058619ebd89ce587c1a584a383d4
GoodGuySteve <q-bert@live.com>
2021-04-19 07:37:58 -0700

    Added first version (only affects distillator sound).

---
+++ migrainefriendlypy_0.1.0/Readme.md
@@ -0,0 +1,5 @@
+This is just a simple fix to the pyanodon mods to manually remove high-frequency sounds from the mod sounds. These 
+sounds trigger migraines for me, so it's mostly just for my personal use.
+
+I don't claim any ownership of the sound files, nor do I make any guarantee that they haven't diminished from their
+original sound quality.
---
+++ migrainefriendlypy_0.1.0/data-updates.lua
@@ -0,0 +1,8 @@
+data.raw['assembling-machine']["distilator"].working_sound.sound.filename = "__migrainefriendlypy__/sounds/distilator.ogg"
+data.raw['assembling-machine']["distilator"].working_sound.idle_sound.filename = "__migrainefriendlypy__/sounds/distilator.ogg"
+data.raw['assembling-machine']["distilator-mk02"].working_sound.sound.filename = "__migrainefriendlypy__/sounds/distilator.ogg"
+data.raw['assembling-machine']["distilator-mk02"].working_sound.idle_sound.filename = "__migrainefriendlypy__/sounds/distilator.ogg"
+data.raw['assembling-machine']["distilator-mk03"].working_sound.sound.filename = "__migrainefriendlypy__/sounds/distilator.ogg"
+data.raw['assembling-machine']["distilator-mk03"].working_sound.idle_sound.filename = "__migrainefriendlypy__/sounds/distilator.ogg"
+data.raw['assembling-machine']["distilator-mk04"].working_sound.sound.filename = "__migrainefriendlypy__/sounds/distilator.ogg"
+data.raw['assembling-machine']["distilator-mk04"].working_sound.idle_sound.filename = "__migrainefriendlypy__/sounds/distilator.ogg"
